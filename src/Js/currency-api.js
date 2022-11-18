fdc653b07b6869e207098ea1eec3e64121f6ea2e
BrandonF55 <brandon.fenk99@gmail.com>
2022-11-18 15:06:18 -0800

    running into errors with the child compilation error

--- src/Js/currency-api.js
+++ src/Js/currency-api.js
@@ -5,12 +5,13 @@ static getCurrency(currency, usd) {
     let request = XMLHttpRequest();
     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency}/${usd}`
     request.addEventListener("loadend", function() {
-      const response = JSON.parse(this.responseText);
-      if ( this.status === 200){
+       const response = JSON.parse(this.responseText);
+      console.log(process.env.API_KEY);
+      if (this.status === 200){
         resolve([response, currency, usd]);
       } else {
         reject([this, response, currency, usd]);
-        console.log(response);
+        
       }
     });
     request.open("GET", url, true);
